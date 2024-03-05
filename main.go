378ea338f7cf11abf659502272241cc5b3daef43
cloudsunset <rodrigues3442@hotmail.com>
2024-03-05 01:07:29 -0300

    routes working

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"github.com/cloudsunset/api-go/src/controller/routes"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	router := gin.Default()
+
+	routes.InitRoutes(&router.RouterGroup)
+
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
+}
