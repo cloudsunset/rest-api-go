ce7bdca68e165f1f4366a211e00429fcddf4d723
cloudsunset <rodrigues3442@hotmail.com>
2024-03-07 22:52:12 -0300

    add logger

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,33 @@
 package controller
 
 import (
-	"github.com/cloudsunset/api-go/src/configuration/rest_err"
+	"github.com/cloudsunset/api-go/src/configuration/logger"
+	"github.com/cloudsunset/api-go/src/configuration/validation"
+	"github.com/cloudsunset/api-go/src/controller/model/request"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Voce chamou a rota de forma errada")
-	c.JSON(err.Code, err)
+
+	logger.Info("Create User Controller",
+		zap.String("journey", "createUser"),
+	)
+
+	var userRequest request.UserRequest
+
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("errors trying to marshal object, error=%s\n", err)
+
+		errRest := validation.ValidateUserError(err)
+
+		c.JSON(errRest.Code, errRest)
+		return
+	}
+
+	logger.Info("User created successfully",
+		zap.String("journey", "createUser"),
+	)
+
+	//c.JSON(http.StatusOK, response)
 }
